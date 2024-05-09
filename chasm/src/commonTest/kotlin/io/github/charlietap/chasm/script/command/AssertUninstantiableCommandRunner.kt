d8bf973dee54e066028d5a054170c8cdabf630a3
CharlieTap <charlie.tapping@deliveroo.co.uk>
2024-05-09 18:33:17 +0100

    implement uninstantiable runner

--- chasm/src/commonTest/kotlin/io/github/charlietap/chasm/script/command/AssertUninstantiableCommandRunner.kt
+++ chasm/src/commonTest/kotlin/io/github/charlietap/chasm/script/command/AssertUninstantiableCommandRunner.kt
@@ -1,12 +1,30 @@
 package io.github.charlietap.chasm.script.command
 
+import com.goncalossilva.resources.Resource
+import io.github.charlietap.chasm.embedding.instance
+import io.github.charlietap.chasm.embedding.module
+import io.github.charlietap.chasm.flatMap
+import io.github.charlietap.chasm.fold
+import io.github.charlietap.chasm.script.ScriptContext
 import io.github.charlietap.sweet.lib.command.AssertUninstantiableCommand
 
-typealias AssertUninstantiableCommandRunner = (AssertUninstantiableCommand) -> CommandResult
+typealias AssertUninstantiableCommandRunner = (ScriptContext, AssertUninstantiableCommand) -> CommandResult
 
 fun AssertUninstantiableCommandRunner(
+    context: ScriptContext,
     command: AssertUninstantiableCommand,
 ): CommandResult {
-    println("ignoring AssertUninstantiableCommand")
-    return CommandResult.Success
+
+    val moduleFilePath = context.binaryDirectory + "/" + command.filename
+    val bytes = Resource(moduleFilePath).readBytes()
+
+    return module(bytes).flatMap { module ->
+        println(module)
+        instance(context.store, module, context.imports)
+    }.fold({
+        println(it)
+        CommandResult.Failure(command, "instantiation succeeded when it shouldn't")
+    }) {
+        CommandResult.Success
+    }
 }
